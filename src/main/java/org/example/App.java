8cc21a62c3646ec578eb175de543dd5db2a5856a
WilliamFun <1509325537@qq.com>
2022-01-11 15:27:59 +0800

    Springboot完成基础项目搭建

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.dao.UserDOMapper;
+import org.example.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration//启动内嵌的tomcat，加载默认的配置
+//@EnableAutoConfiguration//启动内嵌的tomcat，加载默认的配置
 //把该启动类当成可以自动配置的bean，开启整个工程基于Springboot的自动化配置
+@SpringBootApplication(scanBasePackages = {"org.example"})//自动扫描根目录的包，通过注解的方式自动发现spring特定的注解
 @RestController
 //实现MVC一些复杂的配置
+@MapperScan("org.example.dao")//dao存放的路径
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "hello world!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
+
     }
     public static void main( String[] args )
     {
