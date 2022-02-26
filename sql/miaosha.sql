8b88f2d55eb7c014dee97391697431cfcfcba5a4
WilliamFun <1509325537@qq.com>
2022-02-26 17:38:41 +0800

    补充总结文档和数据库

---
+++ sql/miaosha.sql
@@ -0,0 +1,159 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : localhost:3306
+ Source Schema         : miaosha
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 26/02/2022 17:35:06
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for item
+-- ----------------------------
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `price` double(10, 2) NOT NULL DEFAULT 0.00,
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `sales` int NOT NULL DEFAULT 0 COMMENT '异步更新',
+  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of item
+-- ----------------------------
+INSERT INTO `item` VALUES (4, 'iPhone11', 1000.00, '好用', 102, 'https://img1.baidu.com/it/u=304956643,1770721918&fm=26&fmt=auto');
+INSERT INTO `item` VALUES (5, 'iphone2', 111.00, '不好意', 88, 'https://img2.baidu.com/it/u=4168854299,2004779685&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=333');
+
+-- ----------------------------
+-- Table structure for item_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `item_stock`;
+CREATE TABLE `item_stock`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `stock` int NOT NULL DEFAULT 0,
+  `item_id` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of item_stock
+-- ----------------------------
+INSERT INTO `item_stock` VALUES (4, 118, 4);
+INSERT INTO `item_stock` VALUES (5, 339, 5);
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info`  (
+  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int NOT NULL DEFAULT 0,
+  `item_id` int NOT NULL DEFAULT 0,
+  `item_price` double NOT NULL DEFAULT 0,
+  `amount` int NOT NULL DEFAULT 0,
+  `order_price` double NOT NULL DEFAULT 0,
+  `promo_id` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of order_info
+-- ----------------------------
+INSERT INTO `order_info` VALUES ('2022022500000000', 10, 4, 0, 1, 0, 0);
+INSERT INTO `order_info` VALUES ('2022022500000100', 10, 5, 111, 1, 111, 0);
+INSERT INTO `order_info` VALUES ('2022022500000200', 10, 5, 111, 1, 111, 0);
+INSERT INTO `order_info` VALUES ('2022022500000300', 10, 5, 111, 1, 111, 0);
+INSERT INTO `order_info` VALUES ('2022022500000400', 10, 4, 1000, 1, 1000, 0);
+INSERT INTO `order_info` VALUES ('2022022500000500', 10, 4, 1000, 1, 1000, 0);
+INSERT INTO `order_info` VALUES ('2022022600000600', 10, 4, 800, 1, 800, 1);
+
+-- ----------------------------
+-- Table structure for promo
+-- ----------------------------
+DROP TABLE IF EXISTS `promo`;
+CREATE TABLE `promo`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `promo_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `item_id` int NOT NULL DEFAULT 0,
+  `promo_item_price` double NOT NULL DEFAULT 0,
+  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of promo
+-- ----------------------------
+INSERT INTO `promo` VALUES (1, 'iphone8抢购', '2022-02-26 16:32:50', 4, 800, '2022-02-28 15:22:09');
+
+-- ----------------------------
+-- Table structure for sequence_info
+-- ----------------------------
+DROP TABLE IF EXISTS `sequence_info`;
+CREATE TABLE `sequence_info`  (
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `current_value` int NOT NULL DEFAULT 0,
+  `step` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sequence_info
+-- ----------------------------
+INSERT INTO `sequence_info` VALUES ('order_info', 7, 1);
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `gender` tinyint NOT NULL DEFAULT 0 COMMENT '//0代表男性，1代表女性',
+  `age` int NOT NULL DEFAULT 0,
+  `telphone` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `register_mode` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '//byphone,bywechat,byalipay',
+  `third_party_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `telphone_unique_index`(`telphone`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES (1, '第一个用户', 0, 30, '1362224123987', 'byphone', '');
+INSERT INTO `user_info` VALUES (7, 'John', 1, 11, '123', 'byphone', '');
+INSERT INTO `user_info` VALUES (10, 'ZhengFAN', 1, 11, '1234', 'byphone', '');
+
+-- ----------------------------
+-- Table structure for user_password
+-- ----------------------------
+DROP TABLE IF EXISTS `user_password`;
+CREATE TABLE `user_password`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `encrpt_password` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `user_id` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user_password
+-- ----------------------------
+INSERT INTO `user_password` VALUES (1, 'uaghdiuqwgiq', 1);
+INSERT INTO `user_password` VALUES (5, 'lueSGJZetyySpUndWjMBEg==', 7);
+INSERT INTO `user_password` VALUES (6, 'gdyb21LQTcIANtvYMT7QVQ==', 10);
+
+SET FOREIGN_KEY_CHECKS = 1;
